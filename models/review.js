b1037d455c1a51e5648bce41588307e7a9d7df2b
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2017-01-13 13:34:38 -0800

    Comments on models/review.js

--- models/review.js
+++ models/review.js
@@ -10,9 +10,9 @@ var ReviewSchema = new Schema ({
     type: Schema.Types.ObjectId,
     ref: 'Sandwich'
   }],
-  users: [{
-    type: Schema.Types.ObjectId,
-    ref: 'User'
+  users: [{                           // Will a review ever need to store more than one user?
+    type: Schema.Types.ObjectId,      // It seems like each review will have one restaurant, one sandwich, one user. 
+    ref: 'User'                       // In that case, these do not need to be arrays. 
   }],
   restaurants: [{
     type: Schema.Types.ObjectId,
